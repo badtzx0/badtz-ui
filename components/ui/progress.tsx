d753b23350c2b38cb5807392dac46f1dbb40ff15
badtzx0 <admin@badtz-ui.com>
2025-07-28 15:49:03 +0200

    fix: Safari badges opacity

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -14,7 +14,7 @@ function Progress({
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
+        "relative h-2 w-full overflow-hidden rounded-full [background-color:color-mix(in_oklch,var(--primary)_20%,transparent)]",
         className
       )}
       {...props}
